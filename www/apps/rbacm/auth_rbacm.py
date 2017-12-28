12741bc19097b6c974b62e63fe493991b0d5f939
jeremaihloo <1006397539@qq.com>
2017-12-28 13:40:15 +0800

    Fix rbac_auth_provider error(arguments)

--- www/apps/rbacm/auth_rbacm.py
+++ www/apps/rbacm/auth_rbacm.py
@@ -3,5 +3,5 @@ from app_cores import app_fn
 
 
 @app_fn(events.__EVENT_AUTHING__, '', '')
-def auth_rbacm_provider(app, request):
+async def auth_rbacm_provider(app, request):
     return True, 'auth ok'
