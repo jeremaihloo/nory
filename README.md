985ee98bce56e8a1b4e4e0ade39fc8879319e33f
jeremaihloo <1006397539@qq.com>
2018-03-27 13:38:18 +0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Nory
+
+A simple web framework base on aiohttp
+
+## Important
+
+It is a toy for me now !
+
+## Usage
+
+
+## LICENSE
+
+GNU GENERAL PUBLIC LICENSE
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='nory',
     version='1',
-    description='A simple web framework using asyncio',
+    description='A simple web framework base on aiohttp',
     url='http://github.com/jeremaihloo/nory',
     author='jeremaihloo',
     author_email='jeremaihloo1024@gmail.com',
     license='MIT',
-    packages=['nory'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     zip_safe=False,
-    entry_points={
-        'console_scripts': ['nory=nory.hotting:main'],
-    }
+    # entry_points={
+    #     'console_scripts': ['nory=nory.hotting:main'],
+    # }
 )
